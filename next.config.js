013cc93612e31d66272e1a2e168ca97acb125887
Wart Claes <wart.claes@gmail.com>
2023-12-13 10:07:23 +0100

    add reproduction case

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: () => ([
+    {
+      source: '/article/:slug*',
+      destination: '/detail/:slug*',
+      locale: false,
+    },
+  ]),
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,21 @@
+import Link from "next/link";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  const hrefs = [
+    '/detail/foo/bar/123',
+    '/article/foo/bar/123',
+  ];
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      { hrefs.map((href) => (
+        <div key={href}>
+          <Link href={href}>
+            {href}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
